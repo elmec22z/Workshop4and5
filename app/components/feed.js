928000356fc7e07c9bcd0fbc7217e8751966bcb1
elmec22z <elmec22z@mtholyoke.edu>
2016-11-10 17:59:28 -0500

    fb6

--- app/components/feed.js
+++ app/components/feed.js
@@ -1,45 +1,49 @@
 import React from 'react';
 import FeedItem from './feeditem';
 import StatusUpdateEntry from './statusupdateentry';
-import {getFeedData} from '../server';
+import {getFeedData, postStatusUpdate} from '../server';
 
 export default class Feed extends React.Component {
-  constructor(props) {
-    // super() calls the parent class constructor --
-    // e.g. React.Component's constructor.
+  constructor(props){
     super(props);
-    // Set state's initial value.
-    // Note that the constructor is the ONLY place
-    // you should EVER set state directly!
-    // In all other places, use the `setState` method instead.
-    // Setting `state` directly in other places will not
-    // trigger `render()` to run, so your
-    // program will have bugs.
     this.state = {
-    // Empty feed.
-    contents: []
-  };
-}
+      contents: []
+    };
+  }
 
-render() {
-  return (
-    <div>
-      <StatusUpdateEntry />
-      {this.state.contents.map((feedItem) => {
+  refresh() {
+    getFeedData(this.props.user, (feedData) => {
+      this.setState(feedData);
+    });
+  }
+
+  onPost(postContents) {
+    postStatusUpdate(4, "Amherst, MA", postContents, () => {
+      this.refresh();
+    });
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+
+  render() {
+    return (
+      <div>
+        <StatusUpdateEntry
+          onPost={(postContents) => this.onPost(postContents)} />
+        {this.state.contents.map((feedItem) => {
           return (
             <FeedItem key={feedItem._id} data={feedItem} />
-);
-})}
-</div> )
-}
+          );
+        })}
+      </div>
+    )
+  }
 
-componentDidMount() {
-  getFeedData(this.props.user, (feedData) => {
-    // Note: setState does a *shallow merge* of
-    // the current state and the new state. If
-    // state was currently set to {foo: 3}, and
-    // we setState({bar: 5}), state would then be
-    // {foo: 3, bar: 5}. This won't be a problem here.
-    this.setState(feedData);
-}); }
+  componentDidMount() {
+    getFeedData(this.props.user, (feedData) => {
+      this.setState(feedData);
+    });
+  }
 }
--- app/components/feeditem.js
+++ app/components/feeditem.js
@@ -2,78 +2,126 @@ import React from 'react';
 import StatusUpdate from './statusupdate';
 import CommentThread from './commentthread';
 import Comment from './comment';
+import {postComment, unlikeFeedItem, likeFeedItem} from '../server';
+
 export default class FeedItem extends React.Component {
-  render() {
-  var data = this.props.data;
-  var contents;
-  switch(data.type) {
-    case "statusUpdate":
-      // Create a StatusUpdate. Dynamically created
-      // React component: needs a key.
-      // Keys only need to be unique among *siblings*,
-      // so we can re-use the same key as the FeedItem.
+  constructor(props) {
+    super(props);
+    this.state = props.data;
+  }
+
+  handleCommentPost(commentText) {
+    postComment(this.state._id, 4, commentText, (updatedFeedItem) => {
+      this.setState(updatedFeedItem);
+    });
+  }
+
+  handleLikeClick(clickEvent) {
+    clickEvent.preventDefault();
+
+    if(clickEvent.button === 0) {
+
+      var callbackFunction = (updatedLikeCounter) => {
+        this.setState({likeCounter: updatedLikeCounter});
+      };
+
+      if (this.didUserLike()) {
+        unlikeFeedItem(this.state._id, 4, callbackFunction);
+      } else {
+        likeFeedItem(this.state._id, 4, callbackFunction);
+      }
+    }
+  }
+
+  didUserLike() {
+    var likeCounter = this.state.likeCounter;
+    var liked = false;
+    for (var i = 0; i < likeCounter.length; i++) {
+      if (likeCounter[i]._id === 4) {
+        liked = true;
+        break;
+      }
+    }
+
+    return liked;
+  }
+
+  render () {
+    var likeButtonText = "Like";
+    if (this.didUserLike()) {
+      likeButtonText = "Unlike";
+    }
+
+    var data = this.state;
+    var contents;
+    switch(data.type) {
+      case "statusUpdate":
       contents = (
-        <StatusUpdate key={data._id}
-                      author={data.contents.author}
-                      postDate={data.contents.postDate}
-                      location={data.contents.location}>
-          {data.contents.contents}
+        <StatusUpdate key={data._id} author={data.contents.author}
+          postDate={data.contents.postDate} location={data.contents.location}>
+          {data.contents.contents.split("\n").map((line, i) => {
+            return (
+              <p key={"line" + i}>{line}</p>
+            );
+          })}
         </StatusUpdate>
       );
       break;
-    default:
+      default:
       throw new Error("Unknown FeedItem: " + data.type);
-  }
-  return (
-    <div className="fb-status-update panel panel-default">
-      <div className="panel-body">
-        {contents}
-        <hr />
-<div className="row">
-          <div className="col-md-12">
-            <ul className="list-inline">
-              <li>
-              <a href="#">
-                <span className="glyphicon glyphicon-thumbs-up">
-                </span> Like</a>
-</li> <li>
-                <a href="#">
-                <span className="glyphicon glyphicon-comment">
-                </span>
-Comment </a>
-              </li>
-              <li>
-              <a href="#">
-                <span className="glyphicon glyphicon-share-alt">
-                </span> Share</a>
-              </li>
-</ul> </div>
-        </div>
-      </div>
-      <div className="panel-footer">
-        <div className="row">
-          <div className="col-md-12">
-            <a href="#">{data.likeCounter.length} people </a>
-              like this
+    }
+
+    return (
+      <div className="fb-status-update panel panel-default">
+        <div className="panel-body">
+          {contents}
+          <hr />
+          <div className="row">
+            <div className="col-md-12">
+              <ul className="list-inline">
+                <li>
+                  <a href="#" onClick={(e) => this.handleLikeClick(e)}>
+                    <span className="glyphicon glyphicon-thumbs-up"></span>
+                     {likeButtonText}
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span className="glyphicon glyphicon-comment"></span>
+                     Comment
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span className="glyphicon glyphicon-share-alt"></span>
+                     Share
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <hr />
-        <CommentThread>
-          {
-data.comments.map((comment, i) => {
-  // i is comment's index in comments array
-  return (
-    <Comment key={i}
-             author={comment.author}
-             postDate={comment.postDate}>
-      {comment.contents}
-    </Comment>
-  );
-})
-}
-        </CommentThread>
+        <div className="panel-footer">
+          <div className="row">
+            <div className="col-md-12">
+              <a href="#">{data.likeCounter.lenght} people</a> like this
+            </div>
+          </div>
+          <hr />
+          <CommentThread onPost={(commentText) => this.handleCommentPost(commentText)}>
+            {
+            data.comments.map((comment, i) => {
+              return (
+                <Comment key={i} author={comment.author}
+                  postDate={comment.postDate}>
+                  {comment.contents}
+                </Comment>
+              );
+            })
+          }
+          </CommentThread>
+        </div>
       </div>
-    </div>
-)
-    }
+    )
+  }
 }
